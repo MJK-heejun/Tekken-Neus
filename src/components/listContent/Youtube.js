f6ba8c21a916a71e83aa9eecf575d6e32648cbd4
hjk7 <hjk7@sfu.ca>
2017-10-02 01:17:57 -0700

    youtube list can now be retrieved

--- src/components/listContent/Youtube.js
+++ src/components/listContent/Youtube.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 
 class Youtube extends Component {
+
+    constructor(props){
+        super(props);
+        console.log(this.props.data);
+    }
+
     render() {
         return (
             <div>
-                
+                {this.props.data.snippet.title}<br/>
+                {this.props.data.snippet.description}<br/>
+                <img src={this.props.data.snippet.thumbnails.default.url} />
             </div>
         );
     }
--- src/components/tabContent/YoutubeList.js
+++ src/components/tabContent/YoutubeList.js
@@ -1,10 +1,53 @@
 import React, { Component } from 'react';
+import Youtube from '../listContent/Youtube';
+
+const apiKey = "someApiKey";
+const resultNum = "6";
+const channelId = "UC_ntXHv-XdKCD7CPynVvnQw";
+const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${channelId}&maxResults=${resultNum}&q=tekken&key=${apiKey}`;
 
 class YoutubeList extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            requestFailed: false
+        }
+    }
+
+    //automatically call when component mount
+    componentDidMount(){
+        fetch(url)
+            .then(response => {
+                if(!response.ok)
+                    throw Error("Network request failed");
+                return response;
+            })
+            .then(d => d.json())
+            .then(d =>{
+                this.setState({ youtubeList: d });
+                console.log(d);
+            }, () => {
+                this.setState({
+                    requestFailed: true
+                })
+            })
+    }
+
+
     render() {
+        if(this.state.requestFailed) return <p>Network Failed!</p>
+        if(!this.state.youtubeList) return <p>Loading...</p>
+
+        var listItems = this.state.youtubeList.items.map(function(item) {
+            return (
+              <Youtube key={item.id.videoId} data={item}></Youtube>
+            );
+          });
+          
         return (
             <div>
-                this is youtube list
+                {listItems}
             </div>
         );
     }
