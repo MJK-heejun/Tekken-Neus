21fa3516fe40a2688799e7ef0f7f07aff0ae2a58
hjk7 <hjk7@sfu.ca>
2017-10-02 01:20:48 -0700

    readme update + tmp apikey added for convenience

--- src/components/tabContent/YoutubeList.js
+++ src/components/tabContent/YoutubeList.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Youtube from '../listContent/Youtube';
 
-const apiKey = "someApiKey";
+const apiKey = "AIzaSyAUUfDpnPm3K9lhXYWLH6fg0e4nVZjkPxk";
 const resultNum = "6";
 const channelId = "UC_ntXHv-XdKCD7CPynVvnQw";
 const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${channelId}&maxResults=${resultNum}&q=tekken&key=${apiKey}`;
