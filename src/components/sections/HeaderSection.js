3b92849403084cfc0e9e5845c741ece9a7e27166
hjk7 <hjk7@sfu.ca>
2017-10-10 00:24:09 -0700

    service worker transformed into class object inheriting from event emitter

--- src/components/sections/HeaderSection.js
+++ src/components/sections/HeaderSection.js
@@ -6,7 +6,7 @@ import SettingSection from './SettingSection';
 class HeaderSection extends Component {
     constructor(props) {
         super(props);
-        this.state = {open: false};
+        this.state = {open: true};
       }
 
     handleToggle = () => this.setState({open: !this.state.open});
--- src/components/sections/SettingSection.js
+++ src/components/sections/SettingSection.js
@@ -1,7 +1,23 @@
 import React, { Component } from 'react';
-
+import registerServiceWorker from '../../registerServiceWorker';
 
 class SettingSection extends Component {
+
+    constructor(){
+        super();
+        this.state = {
+            swRegistration: null
+        };
+    }
+
+    componentWillMount(){
+        registerServiceWorker.on("change", ()=>{
+            this.setState({
+                swRegistration: registerServiceWorker.swRegistration
+            });
+        });
+    }
+
     render() {
         return (
             <div>
